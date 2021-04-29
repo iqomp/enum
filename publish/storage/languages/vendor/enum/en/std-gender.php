b2599efa473f154bf0a802b68df823e317060f34
Iqbal Fauzi <iqbalfawz@gmail.com>
2021-04-29 11:55:30 +0700

    Re-add translation

---
+++ publish/storage/languages/vendor/enum/en/std-gender.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Unknown'    => 'Unknown',
+    'Male'       => 'Male',
+    'Female'     => 'Female',
+    'Non-Binary' => 'Non-Binary'
+];
---
+++ publish/storage/languages/vendor/enum/id/std-gender.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Unknown'    => 'Tidak diketahui',
+    'Male'       => 'Laki-laki',
+    'Female'     => 'Perempuan',
+    'Non-Binary' => 'Bukan Binari'
+];
